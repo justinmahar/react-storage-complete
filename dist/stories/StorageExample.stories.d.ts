ac108ca823dbc45b19856323aac6cf8cde5bb2d2
Justin Mahar <contact@justinmahar.com>
2023-01-25 00:18:27 -0500

    Update version minor

--- dist/stories/StorageExample.stories.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-declare const _default: ComponentMeta<({ useStorage, ...props }: import("../components/StorageExample").LocalStorageExampleProps<any>) => JSX.Element>;
-export default _default;
-export declare const LocalStorage: ComponentStory<({ useStorage, ...props }: import("../components/StorageExample").LocalStorageExampleProps<any>) => JSX.Element>;
-export declare const SessionStorage: ComponentStory<({ useStorage, ...props }: import("../components/StorageExample").LocalStorageExampleProps<any>) => JSX.Element>;
